b1c7590e503062f584eeee5d07e868b18f255257
JonesQuito <jonesdhy@hotmail.com>
2018-11-02 13:24:07 -0300

    Classificador com texto limpo

--- modulos/teste.py
+++ modulos/teste.py
@@ -33,3 +33,41 @@ def getAllFiles(path):
         return files
     
     return files
+
+
+###############################################
+#nltk.download('punkt')
+#nltk.download('stopwords')
+#nltk.download('rslp') Instala o stemmer português
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import RSLPStemmer
+
+def stemmer(word):
+    stemmer = nltk.stem.RSLPStemmer()
+    return stemmer.stem(word)
+
+
+
+
+def cleaner(text):
+    text = re.sub('[^A-Za-z çáéíóúâôêãõ]', '', text)
+    text = re.sub('  ', ' ', text)
+    text = text.lower()
+
+    tokenized_text = word_tokenize(text)
+
+    stop_words = stopwords.words('portuguese')
+    #stop_words = stopwords.words('english')
+
+    tokens = []
+    for palavra in tokenized_text:
+        if ((palavra not in stop_words) and (len(palavra)>1)):
+            palavra = stemmer(palavra)
+            tokens.append(palavra)
+    
+    return ' '.join(tokens)
+    
